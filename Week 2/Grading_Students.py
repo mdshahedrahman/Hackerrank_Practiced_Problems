b27cfa03bf63ab4007f552cb6faf29ea94c39f8e
Mohammad Shahedur Rahman <147561306+mdshahedrahman@users.noreply.github.com>
2023-11-08 18:30:17 -0600

    Add files via upload

---
+++ Week 2/Grading_Students.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    temp = []
+    for i in grades:
+        if (i <= 37 or (i % 5 <= 2)):
+            temp.append(i)
+        else:
+            k = i % 5
+            if k>=3:
+                temp.append(i + (5-k))
+            else:
+                temp.append(i)
+
+    return temp
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
